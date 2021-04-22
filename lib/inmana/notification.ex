3e4c73d185a030e855e9382f4bf1091166d8be28
Esrom de Paiva <esromqkd@gmail.com>
2021-04-22 12:33:29 -0300

    Adding Task for sending asynchronous email

--- lib/inmana/notification.ex
+++ lib/inmana/notification.ex
@@ -1,14 +1,19 @@
 defmodule Inmana.Supplies.Notification do
+  use Task
   alias Inmana.Mailer
   alias Inmana.Supplies.{ExpirationEmail, ShowByExpiration}
 
   def send do
     data = ShowByExpiration.call()
 
-    Enum.each(data, fn {to_email, supplies} ->
-      to_email
-      |> ExpirationEmail.create(supplies)
-      |> Mailer.deliver_later!()
-    end)
+    data
+    |> Task.async_stream(fn {to_email, supplies} -> send_email(to_email, supplies) end)
+    |> Stream.run()
+  end
+
+  defp send_email(to_email, supplies) do
+    to_email
+    |> ExpirationEmail.create(supplies)
+    |> Mailer.deliver_later!()
   end
 end
